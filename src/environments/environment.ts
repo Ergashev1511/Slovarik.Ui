f0ed35f46b12bc4a7fbd00392334574c041be4bd
t_ergashev <tursunmamatergashev80@gmail.com>
2026-07-22 23:50:09 +0500

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // Backend launchSettings: https profile -> https://localhost:7097, http profile -> http://localhost:5097
-  apiUrl: 'http://localhost:5097/api'
+  apiUrl: 'https://slovarik-api.onrender.com/api'
 };
